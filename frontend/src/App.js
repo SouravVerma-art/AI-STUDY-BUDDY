f2a90e958d90ad3c4928448dece4e9a00115ec7d
Sourav <souravverma769@gmail.com>
2025-09-04 11:42:01 -0700

    Converted frontend from submodule to normal folder

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import StudyBuddyDashboard from "./components/StudyBuddyDashboard";
+
+function App() {
+  return (
+    <div>
+      <StudyBuddyDashboard />
+    </div>
+  );
+}
+
+export default App;
